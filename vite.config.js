3f3d58c5e0b79cd234d8bc948ceb67b97bc929a9
Nanang Setya Wardana <nswardana@gmail.com>
2026-05-05 10:44:58 +0700

    gemilang

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    host: '0.0.0.0', // supaya bisa diakses dari luar (nginx/domain)
+    port: 5550,
+
+    allowedHosts: [
+      'gemilang.beeasy.id',
+      'maqlubah-api.beeasy.id'
+    ]
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 5550,
+    allowedHosts: [
+      'gemilang.beeasy.id',
+      'maqlubah-api.beeasy.id'
+    ]
+  }
 })
